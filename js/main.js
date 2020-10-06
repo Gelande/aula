e51559914c76f8afe1aa4e7cd3505790188238d0
Gelande <agelande@gmail.com>
2020-10-06 09:59:55 -0300

    trabalhando com variáveis e string

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-alert("Meu segundo javascript!");
+//Trabalhando com variáveis:
+var nome = "Alexandre Gelande";
+var num1 = "32";
+var num2 = "8";
+var frase = "Japão é o melhor time do mundo."
+
+//alert("Bem vindo " + nome + "!");
+//alert(num1 + num2);
+
+console.log(frase.replace("Japão", "Brasil")); //alterando string
+console.log(frase.toLowerCase());
