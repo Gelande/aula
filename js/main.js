419eb9f817d1ee9fc74049f7b40a75e231b3000d
Gelande <agelande@gmail.com>
2020-10-06 10:26:36 -0300

    trabalhando com array e dicionário

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 //Trabalhando com variáveis:
+/*
 var nome = "Alexandre Gelande";
 var num1 = "32";
 var num2 = "8";
@@ -9,3 +10,25 @@ var frase = "Japão é o melhor time do mundo."
 
 console.log(frase.replace("Japão", "Brasil")); //alterando string
 console.log(frase.toLowerCase());
+*/
+
+//Array e dicionário:
+
+//var lista = ['maça', 'laranja', 'pera'];
+//lista.push('uva'); //adiciona
+//lista.pop(); // retira
+//console.log(lista.length); //quantidade de elementos
+//alert(lista[1]);
+//console.log(lista.reverse()); //traz os elementos ao contrario
+//console.log(lista);
+//console.log(lista.toString()); //transforma o array p/ string
+//console.log(lista.join(' - ')); //transforma o array p/ string e altera o separador
+
+var fruta = {nome: 'maça', cor: 'vermelha'}; //dicionário
+console.log(fruta);
+console.log(fruta.nome);
+//alert('Cor da fruta ' + fruta.cor);
+
+var frutas = [{nome: 'maça', cor: 'vermelha'}, {nome: 'banana', cor: 'verde'}]; //coleção
+console.log(frutas);
+console.log(frutas[1]);
